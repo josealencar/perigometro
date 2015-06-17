c44b468841de34c03e800e786b0c6db861e3a032
Rodrigo Frohlich <rodrigofcb@gmail.com>
2015-06-17 15:48:03 -0300

    feat. popular os combobox dos filtros de pesquisa com seus respectivos enums

---
+++ src/Perigometro/Perigometro.Dominio1/Ano.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perigometro.Dominio1
+{
+    public enum Ano
+    {
+         [Description("2000")]
+        DoisMil,
+         [Description("2001")]
+        DoisMilUm,
+         [Description("2002")]
+        DoisMilDois,
+         [Description("2003")]
+        DoisMilTres,
+         [Description("2004")]
+        DoisMilQuatro,
+         [Description("2005")]
+        DoisMilCinco,
+         [Description("2006")]
+        DoisMilSeis,
+         [Description("2007")]
+        DoisMilSete,
+         [Description("2008")]
+        DoisMilOito,
+         [Description("2009")]
+        DoisMilNove,
+         [Description("2010")]
+        DoisMilDez,
+         [Description("2011")]
+        DoisMilOnze,
+         [Description("2012")]
+        DoisMilDoze,
+         [Description("2013")]
+        DoisMilTreze
+
+    }
+}
---
+++ src/Perigometro/Perigometro.Dominio1/Lista.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perigometro.Dominio1
+{
+    public class Lista 
+    {
+        public Ano Ano { get; set; }
+        public Clima Clima { get; set; }
+        public Regiao Regiao { get; set; }
+        public Semana Semana { get; set; }
+        public Tipo Tipo { get; set; }
+        public Turno Turno { get; set; }
+        public Veiculo Veiculo { get; set; }
+
+    }
+}

d8024b5e1e6ca60b4e21d8b0655979befa9ef69b
José Alencar de Souza Rocha <jose_alencar_1403@hotmail.com>
2015-06-17 16:47:44 -0300

    feat(action Form) Adicionar arquivo JS para busca por filtros e action no form

---
+++ src/Perigometro/WebApplication1/Scripts/ajaxFiltrosIndex.js
@@ -0,0 +1,11 @@
+﻿function FiltraDadosIndex() {
+    var formulario = $('form[name=filtrosIndex]');
+    $.ajax({
+        url: '/Acidentes/BuscarDadosFiltrados',
+        type: 'POST'
+    }).done(function (res) {
+        console.log(res);
+    }).fail(function (res) {
+        console.log(res);
+    })
+}

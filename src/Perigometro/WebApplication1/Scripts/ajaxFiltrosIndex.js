7984b164ee0983ea6c33f9005063f61d81bdd0b8
José Alencar de Souza Rocha <jose_alencar_1403@hotmail.com>
2015-06-17 23:58:13 -0300

    feat(ajaxFiltrosIndex.js) Adicionar JS para requisição de dados a partir de filtros.

--- src/Perigometro/WebApplication1/Scripts/ajaxFiltrosIndex.js
+++ src/Perigometro/WebApplication1/Scripts/ajaxFiltrosIndex.js
@@ -1,10 +1,17 @@
 ﻿function FiltraDadosIndex() {
     var formulario = $('form[name=filtrosIndex]');
+    var li = formulario.parent().parent().parent();
+    console.log(formulario);
+    console.log(li);
     $.ajax({
         url: '/Acidentes/BuscarDadosFiltrados',
-        type: 'POST'
+        type: 'POST',
+        dataType: 'JSON',
+        data: formulario.serialize()
     }).done(function (res) {
         console.log(res);
+        li.attr('class', 'dropdown yamm-fw');
+        populaHeatmapData(res);
     }).fail(function (res) {
         console.log(res);
     })

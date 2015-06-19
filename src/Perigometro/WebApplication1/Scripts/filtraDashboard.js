eb637040c86a1fe58ee072a25e17669d03d8ef95
Augusto Schiller <augusto.wartchow@gmail.com>
2015-06-18 23:19:20 -0300

    feat(Dashboard): requisitos concluídos (Colocar Loading)

--- src/Perigometro/WebApplication1/Scripts/filtraDashboard.js
+++ src/Perigometro/WebApplication1/Scripts/filtraDashboard.js
@@ -1,6 +1,7 @@
 ﻿var myPieChart;
+var myBarChart;
 
-function inicializa() {
+function inicializaPie() {
     var ctx = document.getElementById("myChart").getContext("2d");
     var dataGrafico = [
         {
@@ -29,6 +30,26 @@ function inicializa() {
         }
     ];
     myPieChart = new Chart(ctx).Pie(dataGrafico, { animateScale: true });
+}
+
+function inicializaBar() {
+    var ctx = document.getElementById("numeros").getContext("2d");
+
+    var dataGrafico = {
+        labels: ["Dia da Semana - SEXTA", "Condição Climática - BOM", "Região - LESTE", "Transporte mais Seguro - Bicileta", "Transporte mais Inseguro - Automóvel"],
+        datasets: [
+            {
+                label: "Estatisticas",
+                fillColor: "rgba(231, 76, 60,1.0)",
+                strokeColor: "rgba(231, 76, 60,1.0)",
+                highlightFill: "rgba(192, 57, 43,1.0)",
+                highlightStroke: "rgba(192, 57, 43,1.0)",
+                data: [2007, 9487, 3447, 109, 9796]
+            }
+        ]
+    };
+
+    myBarChart = new Chart(ctx).Bar(dataGrafico, { barShowStroke: false });
 };
 
 function buscaEstatisticas(ano1, ano2)
@@ -44,11 +65,39 @@ function buscaEstatisticas(ano1, ano2)
         dataType: 'json',
         contentType: 'application/json; charset=utf-8',
     }).done(function (res) {
+        console.log(res);
+
         var mortes = res.Mortes;
+        var clima = res.Clima;
+        var regiao = res.Regiao;
+        var dia = res.Dia;
+        var seguro = res.Seguro;
+        var inseguro = res.Inseguro;
+
         mortes.forEach(function (item, i) {
             myPieChart.segments[i].value = item.Qtde;
             myPieChart.segments[i].label = item.Regiao;
         });
+
+        myBarChart.datasets[0].bars[0].value = dia[0].Qtde;
+        myBarChart.datasets[0].bars[1].value = clima[0].Qtde;
+        myBarChart.datasets[0].bars[2].value = regiao[0].Qtde;
+        myBarChart.datasets[0].bars[3].value = seguro.Qtde;
+        myBarChart.datasets[0].bars[4].value = inseguro.Qtde;
+
+        myBarChart.datasets[0].bars[0].label = "Dia da Semana - " + dia[0].Dia;
+        myBarChart.scale.xLabels[0] = "Dia da Semana - " + dia[0].Dia;
+        myBarChart.datasets[0].bars[1].label = "Condição Climática - " + clima[0].Clima;
+        myBarChart.scale.xLabels[1] = "Condição Climática - " + clima[0].Clima;
+        myBarChart.datasets[0].bars[2].label = "Região - " + regiao[0].Regiao;
+        myBarChart.scale.xLabels[2] = "Região - " + regiao[0].Regiao;
+        myBarChart.datasets[0].bars[3].label = "Transporte mais Seguro - " + seguro.Nome;
+        myBarChart.scale.xLabels[3] = "Transporte mais Seguro - " + seguro.Nome;
+        myBarChart.datasets[0].bars[4].label = "Transporte mais Inseguro - " + inseguro.Nome;
+        myBarChart.scale.xLabels[4] = "Transporte mais Inseguro - " + inseguro.Nome;
+
+
+        myBarChart.update();
         myPieChart.update();
     })
 }
